cdd6fd2d1ad163ed2c1d39fdf9f9910ff9dd6490
haley <haley@DESKTOP-93K8LIV>
2021-06-17 18:57:23 -0500

    I did the first 4 things in module 2 :3
         ..
          w

--- src/_01_random/_4_validation/Validation.java
+++ src/_01_random/_4_validation/Validation.java
@@ -17,7 +17,19 @@ public class Validation {
 		System.out.println(randomNumber);
 
 		// 1. Use each value of randomNumber to give the user a random compliment.
-
+		if (randomNumber == 1) {
+			JOptionPane.showMessageDialog(null,"you are nice." );
+		} else if (randomNumber == 2) {
+			JOptionPane.showMessageDialog(null,"to too twoooooooooooooooooooo" );
+		} else if (randomNumber == 3) {
+			JOptionPane.showMessageDialog(null,"their there they're" );
+		} else if (randomNumber == 4) {
+			JOptionPane.showMessageDialog(null,"your dog barks too much. :)" );
+		} else if (randomNumber == 5) {
+			JOptionPane.showMessageDialog(null,"you have an AMAZING dog." );
+		} else {
+			JOptionPane.showMessageDialog(null,"you look good." );
+		}
 		// 2. Repeat all the code above 10 times
 		
 		// 3. Find someone to test out your program. They will like it :)
